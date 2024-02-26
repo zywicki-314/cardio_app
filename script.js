2d5c8b61c852fed71288be030afebc199d347d09
Pawel <p3.14zywicki@gmail.com>
2024-02-26 16:12:17 +0100

    cardio_app-248

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+        function(position){
+            const {latitude} = position.coords;
+            const {longitude} = position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`)
+        },
+        function(){
+            alert("Brak dostępu do GPS")
+        }
+        )
+}
