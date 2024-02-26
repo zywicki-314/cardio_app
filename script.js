97bf752d4f278b8184d14755b6e6515d74e489c0
Pawel <p3.14zywicki@gmail.com>
2024-02-26 16:46:31 +0100

    cardio_app-249

--- script.js
+++ script.js
@@ -14,7 +14,19 @@ if (navigator.geolocation){
             const {latitude} = position.coords;
             const {longitude} = position.coords;
             console.log(`https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`)
-        },
+
+            const coords = [latitude, longitude];
+
+            const map = L.map('map').setView(coords, 15);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+            .openPopup();
+            },
         function(){
             alert("Brak dostępu do GPS")
         }
